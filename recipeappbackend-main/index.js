95be399382d62da87335d2140e409a97da843065
Roshan Baidar <roshanbaidar1234@gmail.com>
2025-03-02 13:45:27 +0545

    frontfile

--- recipeappbackend-main/index.js
+++ recipeappbackend-main/index.js
@@ -8,24 +8,24 @@ const port = 5002;
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.status(200).json("server start");
 });
 
 // user routes
 const userAuthRoutes = require("./routes/users/userAuthroutes");
-app.use("/userauth/api",userAuthRoutes);
+app.use("/userauth/api", userAuthRoutes);
 
 
 // recipe routes
 const reciperoutes = require("./routes/recipes/recipeAuthRoutes");
-app.use("/recipe/api",reciperoutes);
+app.use("/recipe/api", reciperoutes);
 
 // review routes
 const reviewroutes = require("./routes/reviews/reviewAuthRoutes");
-app.use("/review/api",reviewroutes);
+app.use("/review/api", reviewroutes);
 
-// server start
-app.listen(port,()=>{
+// server starttt
+app.listen(port, () => {
     console.log(`server start at port no ${port}`)
 })
