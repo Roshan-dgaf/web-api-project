a59a96ac87ed811a8bacb2af70538296ed72f72e
Roshan Baidar <roshanbaidar1234@gmail.com>
2025-03-03 00:09:12 +0545

    backend fix

---
+++ recipeappbackend-main/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const ConnectDB = async()=>{
+    try {
+        const {connection} = await mongoose.connect(process.env.MONGO_URL)
+        console.log(`MongoDB is connected at : ${connection.host}`)
+        
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+        
+    }
+}
+
+export default ConnectDB
